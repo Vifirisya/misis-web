628c3ebd1d50ad4a35f55cc5b03edd677462b29c
Vifirisya <timstov07@gmail.com>
2022-10-27 20:07:13 +0300

    css and js modified

---
+++ project/script.js
@@ -0,0 +1,28 @@
+function acceptForm(){
+        var nm = document.getElementById("nameInput").value;
+        var lastNm = document.getElementById("lastNameInput").value;
+        var pasw = document.getElementById("passwordInput").value;
+        var mail = document.getElementById("emailInput").value;
+        
+        var regexp = /[a-z]/;
+        if (!regexp.test(nm) || !regexp.test(lastNm)){
+            alert("Name and lastname must not contain any numbers!")
+            return
+        }
+
+        regexp = /^[0-9a-z]{5,100}/i;
+
+        if(!regexp.test(pasw)){
+            alert("Password must be from 5 to 100 symbols length!")
+            return
+        }
+
+        alert("Success")
+
+        /*alert(nm);
+        alert(lastNm);
+        alert(pasw);
+        alert(mail);*/
+    }
+
+document.querySelector(".button").addEventListener("click", acceptForm);
